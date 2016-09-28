8a185b0220dee0d65dbeb678c05cd05de2f0c9d0
tianzhiqi <qintianzhiqi@hotmail.com>
2016-09-28 18:03:40 +0800

    list页面待修改

--- src/components/hairItem.jsx
+++ src/components/hairItem.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../assets/css/hair.css'
 
-const hairSrc = require('../assets/img/one-px.jpg')
 
 export default class HairItem extends React.Component {
   constructor(props) {
@@ -12,15 +11,22 @@ export default class HairItem extends React.Component {
   }
   render() {
     return (
-      <div className="hair-item">
-        <a>
-          <img src={hairSrc} />
-          <div className="lazy-img" style={{backgroundImage: 'url('+this.state.imageDomain+this.props.item.coverImg+')'}} />
+      <div className="topic-item">
+        <a className="user-avatar pull-l">
+          <img src={this.props.item.author.avatar_url} title={this.props.item.author.loginname} />
         </a>
-        <div className="hair-name">
-          <span>{this.props.item.albumName}</span>
-          <div className="hair-like">0</div>
+        <span className="reply-count">
+          <span className="count-of-reply" title="回复数">{this.props.item.reply_count}</span>
+          <span className="count-split">/</span>
+          <span className="count-of-click" title="点击数">{this.props.item.visit_count}</span>
+        </span>
+        <div className="topic-title-wrapper">
+          <span className="put-top"></span>
+          <a className="topic-title" title={this.props.item.title}>{this.props.item.title}</a>
         </div>
+        <a className="last-time">
+          <img></img>
+        </a>
       </div>
     )
   }
