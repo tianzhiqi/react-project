483365eaf1241d0bf488fbbaaf554cbf967074a9
tianzhiqi <qintianzhiqi@hotmail.com>
2016-11-04 23:34:14 +0800

    menu

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,6 +5,13 @@ export default class Header extends React.Component {
   render() {
     return (
       <header className="clearfix">
+        <div className="site-nav pull-l">
+          <button>
+            <span className="btn-bar" />
+            <span className="btn-bar" />
+            <span className="btn-bar" />
+          </button>
+        </div>
         <span className="pull-r">
           <a>登录</a>
           <a>注册</a>
