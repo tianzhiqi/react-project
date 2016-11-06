c823f4817f5d9c769dcf232ae42db9b1dd94c7d1
tianzhiqi <qintianzhiqi@hotmail.com>
2016-11-06 23:50:50 +0800

    sidenav

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -13,6 +13,25 @@ export default class Header extends React.Component {
           </button>
           <span className="current-title">{this.props.title}</span>
         </div>
+        <div className="side-nav">
+          <div>
+            <div className="user-view">
+              <div className="user-background">
+                <img src="../assets/img/avatar.png" />
+              </div>
+              <a><img src="../assets/img/center-avatar.png" /></a>
+              <a>David</a>
+            </div>
+            <div className="side-nav-list">
+              <a>全部</a>
+              <a>精华</a>
+              <a>问答</a>
+              <a>分享</a>
+              <a>招聘</a>
+            </div>
+          </div>
+        </div>
+        <div className="overlay" />
       </header>
     )
   }
